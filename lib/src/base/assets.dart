e6fdf4052fb86394ea559d8f7c2e0b23127b583d
arish <arishkhan28.08@gmail.com>
2021-02-19 15:28:11 +0500

    Added main product images

--- lib/src/base/assets.dart
+++ lib/src/base/assets.dart
@@ -1,8 +1,15 @@
 const _iconsDir = 'assets/icons';
+const _imagesDir = 'assets/images';
 
 abstract class Assets {
-  static const aboutIcon = '$_iconsDir/about.png';
+  /// TODO: Change the asset name to `contact-us.png`
+  static const contactIcon = '$_iconsDir/about.png';
   static const cartIcon = '$_iconsDir/cart.png';
   static const dropIcon = '$_iconsDir/drop.png';
   static const folderIcon = '$_iconsDir/folder.png';
+
+  static const nbqFast = '$_imagesDir/nbq_fast.png';
+  static const nbqPropulse = '$_imagesDir/nbq_propulse.png';
+  static const nbqSlow = '$_imagesDir/nbq_slow.png';
+  static const nbqWTF = '$_imagesDir/nbq_wtf.png';
 }
