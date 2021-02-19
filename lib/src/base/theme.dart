6b6dce9acf3eacf10dde7f8152e7e5f35f605b99
arish <arishkhan28.08@gmail.com>
2021-02-19 13:12:36 +0500

    added `primaryColor` as per the design

--- lib/src/base/theme.dart
+++ lib/src/base/theme.dart
@@ -1,6 +1,10 @@
 part of '../app.dart';
 
 abstract class AppTheme {
+  static const primaryColor = Color(0xFFDE007B);
+
   static final _light = ThemeData(
+    primaryColor: primaryColor,
+    accentColor: primaryColor,
   );
 }
