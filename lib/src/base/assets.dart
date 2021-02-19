5990c76ba87d5e0fd793100a3fc76011291c5fea
arish <arishkhan28.08@gmail.com>
2021-02-19 12:56:25 +0500

    :heavy_plus_sign: added nav-bar assets

---
+++ lib/src/base/assets.dart
@@ -0,0 +1,8 @@
+const _iconsDir = 'assets/icons';
+
+abstract class Assets {
+  static const aboutIcon = '$_iconsDir/about.png';
+  static const cartIcon = '$_iconsDir/cart.png';
+  static const dropIcon = '$_iconsDir/drop.png';
+  static const folderIcon = '$_iconsDir/folder.png';
+}
