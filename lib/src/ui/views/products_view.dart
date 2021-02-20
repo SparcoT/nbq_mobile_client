a52b0a1afe8d4723b7ebb67db2ac665fecc6add8
arish <arishkhan28.08@gmail.com>
2021-02-20 20:08:11 +0500

    :art: cleaned and minified [ProductsView]

--- lib/src/ui/views/products_view.dart
+++ lib/src/ui/views/products_view.dart
@@ -1,38 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nbq_mobile_client/src/ui/widgets/product_tile.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:nbq_mobile_client/src/app.dart';
-import 'package:nbq_mobile_client/src/base/assets.dart';
+import 'package:nbq_mobile_client/src/ui/widgets/category_tile.dart';
 
 class ProductsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
       child: Column(children: [
-        ProductTile.slow(),
-        ProductTile.fast(),
-        ProductTile.wtf(),
-        ProductTile.pro(),
+        CategoryTile.slow(),
+        CategoryTile.fast(),
+        CategoryTile.wtf(),
+        CategoryTile.pro(),
       ]),
     );
   }
-}
-
-class InfoBox extends Container {
-  InfoBox(String info)
-      : super(
-          width: 45,
-          height: 25,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(6),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 1),
-                  color: Colors.grey.shade300,
-                )
-              ]),
-          child: Center(child: Text(info)),
-        );
 }
